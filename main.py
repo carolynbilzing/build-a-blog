c08ac3c968e4dee7b83621b0b2c167b9c07386b3
Carolyn Bilzing <carolynbilzing@msn.com>
2016-09-19 19:06:59 -0400

    saving changes

--- main.py
+++ main.py
@@ -14,25 +14,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello darkness my old friend!')
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+
+class Story(db.Model):
+    title = db.StringProperty(required=True)
+    story = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    """docstring for St"""
+
+    # replace this with some code to handle the request,m
+
+
+class Handler(webapp2.RequestHandler):
+    # def get(self):
+    # self.response.write()
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
 
 class ViewPostHandler(webapp2.RequestHandler):
-	"""docstring for ViewPostHandler"""
-	def get(self, id):
-		self.response.write( )
-		pass #replace this with some code to handle the request
+    """docstring for ViewPostHandler"""
+    def get(self, id):
+        story = Story.get_by_id(int(id))
+        self.response.write(story.title)
+
+
+class MainPageHandler(Handler):
+    def render_blog(self, title="", story="", error=""):
+        storys = db.GqlQuery("SELECT * FROM Story ORDER BY created DESC")
+
+        self.render("blog.html", storys=storys)
+
+    def get(self):
+        self.render_blog()
+
+    def post(self):
+        title = self.request.get("title")
+        story = self.request.get("story")
+
+        if title and story:
+            a = Story(title=title, story=story)
+            a.put()
+            self.redirect("/")
+
+        else:
+            error = "we need both a title and a story!"
+            self.render_blog(title, story, error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPageHandler),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
-#webapp2.Route('/blog/<id:\d+>',ViewPostHandler):
-
-
-
-		
-
-
